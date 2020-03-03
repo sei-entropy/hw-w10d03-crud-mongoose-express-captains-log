328ed01f2751c66d164b65c60022a4fd38cf0ed7
Ralabdulwa98 <rmalabdulwahab@gmail.com>
2020-03-03 21:24:19 +0300

    Move schema from server.js to log.js

--- captains_log/server.js
+++ captains_log/server.js
@@ -1,13 +1,3 @@
 
-const mongoose = require('mongoose');
 
-const Schema = new mongoose.Schema({
-    title:  { type: String, required: true },
-    entry:  { type: String, required: true },
-    shipIsBroken: Boolean ,
-});
-
-
-const Login = mongoose.model('The log schema', Schema);
-module.exports = Login;
 
