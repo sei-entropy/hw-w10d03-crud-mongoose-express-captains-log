7f1ae1aa0883fc50c3eec5422df9654b210d5583
malsaadan <mmalsaadan@gmail.com>
2020-03-11 02:59:47 +0300

    Add comments to the solution

--- captains_log/models/log.js
+++ captains_log/models/log.js
@@ -1,9 +1,11 @@
-const mongoose = require('mongoose');
-const logSchema = new mongoose.Schema({
-    title:  { type: String, required: true },
-    entry:  { type: String, required: true },
-    shipIsBroken: true,
-    timestamps: true,
-});
-const Log = mongoose.model('log-collection', logSchema);
+const mongoose = require("mongoose");
+const logSchema = new mongoose.Schema(
+  {
+    title: { type: String, required: true },
+    entry: { type: String, required: true },
+    shipIsBroken: { type: Boolean, default: true }
+  },
+  { timestamps: true }
+);
+const Log = mongoose.model("log-collection", logSchema);
 module.exports = Log;
--- captains_log/server.js
+++ captains_log/server.js
@@ -1,64 +1,81 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const Logs = require('./models/log.js');
+// Require necessary NPM packages
+const express = require("express");
+const mongoose = require("mongoose");
 
+// Requires model
+const Logs = require("./models/log.js");
+
+// Instantiate Express Application Object
 const app = express();
 
 // to get the body of the json
 app.use(express.json());
 
 //... and then farther down the file
-mongoose.connect('mongodb://localhost:27017/basket', { useNewUrlParser: true});
-mongoose.connection.once('open', ()=> {
-    console.log('CONNECTED TO MONGO SUCCESS');
+mongoose.connect("mongodb://localhost:27017/log-app", {
+  useNewUrlParser: true
+});
+mongoose.connection.once("open", () => {
+  console.log("CONNECTED TO MONGO SUCCESS");
 });
 
-app.get('/logs', (req, res) => {
-  Logs.find({}, (error, result)=>{
-      if(error)
-      console.log(error);
-      else
-      res.send(result);
-  })
+// get all the logs
+app.get("/logs", (req, res) => {
+  Logs.find({}, (error, result) => {
+    // if error occur
+    if (error) console.log(error);
+    // show all logs
+    else res.send(result);
+  });
 });
 
-app.get('/logs/:id', (req, res) => {
-    Logs.findById({id:req.params.id}, (error, result)=>{
-        if(error)
-        console.log(error);
-        else
-        res.send(result);
-    })
+// get a specific log by its id
+app.get("/logs/:id", (req, res) => {
+  // Find a log by its id
+  Logs.findById({ id: req.params.id }, (error, result) => {
+    // if error occur
+    if (error) console.log(error);
+    // show the specified log
+    else res.send(result);
   });
+});
 
-  app.post('/logs', (req, res) => {
-    Logs.create(req.body, (error, result)=>{
-        if(error)
-        console.log(error);
-        else
-        res.send(result);
-    })
+// post a log
+app.post("/logs", (req, res) => {
+  Logs.create(req.body.log, (error, result) => {
+    // If error occur
+    if (error) console.log(error);
+    // Show the posted log
+    else res.send(result);
   });
+});
 
-  app.put('/logs/:id', (req, res) => {
-      Logs.findByIdAndUpdate(req.params.id, (error, result) => {
-          if(error)
-          console.log('error: ', error);
-          else
-          res.send(result);
-      })
+// Update logs by their ids
+app.put("/logs/:id", (req, res) => {
+    // Find a specific log by its id
+  Logs.findByIdAndUpdate(req.params.id, (error, result) => {
+      // if error occur
+    if (error) console.log("error: ", error);
+    // show updated log
+    else res.send(result);
   });
+});
 
-  app.delete('/logs', (req, res) => {
-      Logs.findByIdAndRemove(req.params.id, (error, result) => {
-          if(error)
-          console.log("error: ", error);
-          else
-          res.send(result);
-      });
-  })
+// Delete a log
+app.delete("/logs", (req, res) => {
+    // find a log by its id and remove it
+  Logs.findByIdAndRemove(req.params.id, (error, result) => {
+      // if error occur
+    if (error) console.log("error: ", error);
+    // show deleted log
+    else res.send(result);
+  });
+});
 
+// Specify a port
 const PORT = 5000;
+
+// Make the app listen to the port
 app.listen(PORT, () => {
   console.log(`SERVER LISTENING ON PORT http://localhost:${PORT}`);
 });
